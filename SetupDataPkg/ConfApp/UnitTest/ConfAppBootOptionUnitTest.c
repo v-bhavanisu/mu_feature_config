070ca65c085204399d4ae0a3653cb023c5b8c115
Bhavani Subramanian <v-bhavanisu@microsoft.com>
2025-06-19 12:15:02 -0700

    Modified UnitTest cases which uses ResetCold and JumpBuf

--- SetupDataPkg/ConfApp/UnitTest/ConfAppBootOptionUnitTest.c
+++ SetupDataPkg/ConfApp/UnitTest/ConfAppBootOptionUnitTest.c
@@ -107,31 +107,6 @@ EfiBootManagerConnectAll (
   return;
 }
 
-STATIC
-VOID
-EFIAPI
-MockResetSystem (
-  IN EFI_RESET_TYPE  ResetType,
-  IN EFI_STATUS      ResetStatus,
-  IN UINTN           DataSize,
-  IN VOID            *ResetData OPTIONAL
-  )
-{
-  DEBUG ((DEBUG_ERROR, "%a \n", __func__));
-
-  check_expected (ResetType);
-
-  ASSERT (FALSE);
-  return;
-}
-
-///
-/// Mock version of the UEFI Runtime Services Table
-///
-EFI_RUNTIME_SERVICES  MockRuntime = {
-  .ResetSystem = MockResetSystem,
-};
-
 /**
   Mock version of EfiBootManagerGetLoadOptions.
 
@@ -478,12 +453,10 @@ ConfAppBootOptSelectOne (
   expect_memory (EfiBootManagerBoot, BootOption, &BootOption, sizeof (EFI_BOOT_MANAGER_LOAD_OPTION));
   will_return (EfiBootManagerBoot, EFI_SUCCESS);
 
-  // will_return (ResetCold, &JumpBuf);
   expect_value (MockResetSystem, ResetType, EfiResetCold);
 
-  if (!SetJump (&JumpBuf)) {
-    BootOptionMgr ();
-  }
+  Status = BootOptionMgr ();
+  UT_ASSERT_NOT_EFI_ERROR (Status);
 
   return UNIT_TEST_PASSED;
 }
@@ -552,12 +525,10 @@ ConfAppBootOptSelectMore (
   expect_memory (EfiBootManagerBoot, BootOption, &BootOption[1], sizeof (EFI_BOOT_MANAGER_LOAD_OPTION));
   will_return (EfiBootManagerBoot, EFI_SUCCESS);
 
-  // will_return (ResetCold, &JumpBuf);
   expect_value (MockResetSystem, ResetType, EfiResetCold);
 
-  if (!SetJump (&JumpBuf)) {
-    BootOptionMgr ();
-  }
+  Status = BootOptionMgr ();
+  UT_ASSERT_NOT_EFI_ERROR (Status);
 
   return UNIT_TEST_PASSED;
 }
