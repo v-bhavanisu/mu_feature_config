d81456a57c7a3672f5934bda3aca0af8e1eed5ca
Bhavani Subramanian <v-bhavanisu@microsoft.com>
2025-06-23 10:21:11 -0700

    Removed KeyData3

--- SetupDataPkg/ConfApp/UnitTest/ConfAppUnitTest.c
+++ SetupDataPkg/ConfApp/UnitTest/ConfAppUnitTest.c
@@ -379,7 +379,7 @@ ConfAppEntrySelect1 (
 {
   EFI_KEY_DATA  KeyData1;
   EFI_KEY_DATA  KeyData2;
-  EFI_KEY_DATA  KeyData3;
+  // EFI_KEY_DATA  KeyData3;
   EFI_STATUS    Status;
 
   // EFI_KEY_DATA  DummyKey = { 0 };
