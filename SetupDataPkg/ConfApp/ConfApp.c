27b13b71ab77b8cc4a02aef2a1cb6a7ec578a6b9
Bhavani Subramanian <v-bhavanisu@microsoft.com>
2025-07-08 15:31:45 -0700

    ResetSystemWithSubtype used instead of ResetSystem

--- SetupDataPkg/ConfApp/ConfApp.c
+++ SetupDataPkg/ConfApp/ConfApp.c
@@ -417,7 +417,8 @@ ConfAppEntry (
         {
           // Prepare ResetData GUID
           CopyGuid (&ResetData.ResetGuid, &gConfAppResetGuid);
-          gRT->ResetSystem (EfiResetCold, EFI_SUCCESS, sizeof (ResetData), &ResetData);
+          // gRT->ResetSystem (EfiResetCold, EFI_SUCCESS, sizeof (ResetData), &ResetData);
+          ResetSystemWithSubtype (EfiResetCold, &ResetData.ResetGuid);
           Print (L"reset done...\n");
         } else {
           mConfState = MainInit;
